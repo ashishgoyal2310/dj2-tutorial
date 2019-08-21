d545c06f448bbe81198359b0b798cfdde4ed9627
Ashish Goyal <agoyal@q3tech.com>
2019-08-21 14:45:48 +0530

    Update for celery integration and config

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
