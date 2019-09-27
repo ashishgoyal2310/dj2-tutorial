56e025eab3b755468fa12b9de4d2c500f5c22f85
Ashish Goyal <agoyal@q3tech.com>
2019-09-27 15:17:29 +0530

    updated for basic home page

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+
+class HomeView(TemplateView):
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
