867a27638e598d0aa16fc766587dba45a5fc1886
Ashish Goyal <agoyal@q3tech.com>
2019-09-27 14:49:23 +0530

    update to download a file from media

--- app/trackers/urls.py
+++ app/trackers/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from app.trackers.views import DownloadTrackerView
+from app.trackers.views import download
 
 urlpatterns = [
     path('', DownloadTrackerView.as_view(), name='index'),
+    path('download', download, name='download'),
 ]
--- app/trackers/views.py
+++ app/trackers/views.py
@@ -1,4 +1,7 @@
+import os
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.http import HttpResponse, Http404
 
 from django.core.cache import caches
 cache = caches['default']
@@ -15,3 +18,20 @@ class DownloadTrackerView(TemplateView):
         context['tracker_task_ids'] = tracker_task_ids
 
         return context
+
+
+def download(request):
+    filename = request.GET.get('filename', None)
+    if not filename:
+        return HttpResponse('No media file')
+
+    file_path = os.path.join(settings.MEDIA_ROOT, filename)
+    if not os.path.exists(file_path):
+        return HttpResponse('Invalid file name')
+
+    response = None
+    with open(file_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type="application/vnd.ms-excel")
+        response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+
+    return response
