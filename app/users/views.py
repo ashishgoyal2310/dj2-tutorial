86fdd46420ea10b24930f26094325c341d0c8693
Ashish Goyal <a.sh2310goyal@gmail.com>
2020-04-04 20:33:50 +0530

    Update for settings for external template path

--- app/users/views.py
+++ app/users/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, Http404
 class HomeView(TemplateView):
 
     template_name = "home.html"
+    template_name = "demo-nav-bar.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
