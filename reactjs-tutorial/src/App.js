610fc2f2753f5dad39845fc7a5aa863f3d016b95
Ashish Goyal <ashish2.goyal@orange.com>
2020-04-12 17:24:11 +0530

    Update for reactjs App component

--- reactjs-tutorial/src/App.js
+++ reactjs-tutorial/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios'
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         Hello World!!
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // state
+    this.state = {
+      users: []
+    }
+  }
+
+  getUser() {
+    axios.get('https://api.randomuser.me/?results=5&nat=US').then((response) => 
+      {
+        console.log(this, this.state.users.length, response.data.results)
+        this.setState({
+          users: response.data.results
+        })
+      }
+    );
+  }
+
+  componentWillMount() {
+    this.getUser();
+  }
+
+  render() {
+    return (
+      <div className="App App-header"> 
+          
+          {this.state.users.length ? 
+            this.state.users.map((dct) => 
+              <div>
+                <h3>
+                  <img src={dct.picture.thumbnail} /> {dct.name.first} {dct.name.last}
+                </h3>
+                <p>{dct.email}</p>
+                <hr />
+              </div>
+            ) : 'loading...'
+          }
+
+      </div>
+    )
+  }
 }
 
 export default App;
