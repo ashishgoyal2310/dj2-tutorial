9253bbcde10c22f7dac239298b36adf11f91668e
Ashish Goyal <ashish2.goyal@orange.com>
2020-04-19 13:25:55 +0530

    Update for bootstrap CSS

--- reactjs-tutorial/src/index.js
+++ reactjs-tutorial/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import App from "./components/App";
 
 ReactDOM.render(
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
